c96c4df5b49f39a919e9b3346e5a9995901b43e2
Aleksas Pielikis <ant.kampo@gmail.com>
2019-10-20 17:53:00 +0300

    a

--- re_map/core.py
+++ re_map/core.py
@@ -3,13 +3,18 @@ from math import ceil, floor
 from .utils import decorate
 from .fast import intersect, intersection, span_len_delta, span_length, span_rtrim
 
-def span_offset(span, replacement_span_map):
-    delta_start, delta_end = 0, 0
-    for span_source, span_target, _ in replacement_span_map:
+def span_offset(span, replacement_span_map, delta_start=0, delta_end=0, delta_i=0):
+    cached_delta_start = 0
+    cached_delta_end = 0
+    cached_delta_count = 0
+    for span_source, span_target, _ in replacement_span_map[delta_i:]:
         if span_target[1] <= span[0]:
             d = span_len_delta(span_target, span_source)
-            delta_end += d
             delta_start += d
+            delta_end += d
+            cached_delta_start = delta_start
+            cached_delta_end = delta_end
+            cached_delta_count += 1
         else:
             target_trimmed_end = span_rtrim(span_target, span[1])
             if target_trimmed_end:
@@ -25,7 +30,7 @@ def span_offset(span, replacement_span_map):
             else:
                 break
 
-    return delta_start, delta_end
+    return delta_start, delta_end, cached_delta_start, cached_delta_end, delta_i + cached_delta_count
 
 def insert(entry, replacement_span_map, allow_intersect=True):
     def validate(source_span, ref_source_span):
@@ -97,7 +102,7 @@ def repl(match, replacement_map, replacement_span_map):
     match_start = match.span(0)[0]
     if len(match.regs) == 1:
         raise Exception('No match groups in regex pattern.')
-    delta = span_offset(match.span(1), replacement_span_map)
+    _, delta, _, _, _ = span_offset(match.span(1), replacement_span_map)
 
     current_match_delta = 0
 
@@ -108,7 +113,7 @@ def repl(match, replacement_map, replacement_span_map):
         replacement = replacement_map[i] if isinstance(replacement_map[i], str) else replacement_map[i](match.group(i))
         match_string = match_string[0:group_rel_span[0] + current_match_delta] + replacement + match_string[group_rel_span[1] + current_match_delta:]
 
-        match_delta = delta[1] + current_match_delta
+        match_delta = delta + current_match_delta
         group_rel_span_alligned = group_rel_span[0] + match_delta, group_rel_span[1] + match_delta
 
         span_target = group_rel_span_alligned[0] + match_start, group_rel_span_alligned[0] + len(replacement) + match_start
@@ -124,9 +129,12 @@ def normalize_source_spans(replacement_span_map, tmp_replacement_span_map):
     '''
     Corrects the source spans according to earlier length changes
     '''
+    cached_delta_start = 0
+    cached_delta_end = 0
+    delta_i = 0
     for i, (tmp_source_span, _, len_delta) in enumerate(tmp_replacement_span_map):
-        delta_span = span_offset(tmp_source_span, replacement_span_map)
-        tmp_replacement_span_map[i] = (tmp_source_span[0] - delta_span[0], tmp_source_span[1] - delta_span[1]), tmp_replacement_span_map[i][1], len_delta
+        delta_start, delta_end, cached_delta_start, cached_delta_end, delta_i = span_offset(tmp_source_span, replacement_span_map, cached_delta_start, cached_delta_end, delta_i)
+        tmp_replacement_span_map[i] = (tmp_source_span[0] - delta_start, tmp_source_span[1] -delta_end), tmp_replacement_span_map[i][1], len_delta
 
 def update_span_map(replacement_span_map, tmp_replacement_span_map):
     for entry in tmp_replacement_span_map:
